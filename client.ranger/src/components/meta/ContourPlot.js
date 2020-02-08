eead2f8981c70ae9600d78b3f822cdf19137f891
Jake Mirra <jpluto909@gmail.com>
2020-02-07 22:06:18 -0600

    Proofreading.

--- client.ranger/src/components/meta/ContourPlot.js
+++ client.ranger/src/components/meta/ContourPlot.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class NeuralFunctionPlotContainer extends Component {
+export default class ContourPlot extends Component {
 
   componentDidMount() {
     this.plot();
